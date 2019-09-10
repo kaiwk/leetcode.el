044fd1cd0bc727d888b424ffc5a81b22c7b802c1
Wang Kai <kaiwkx@gmail.com>
2019-09-10 10:21:09 +0800

    fix: invalid function when login

--- leetcode.el
+++ leetcode.el
@@ -6,7 +6,7 @@
 ;; Keywords: extensions, tools
 ;; URL: https://github.com/kaiwk/leetcode.el
 ;; Package-Requires: ((emacs "26") (dash "2.16.0") (graphql "0.1.1") (spinner "1.7.3") (aio "1.0"))
-;; Version: 0.1.5
+;; Version: 0.1.6
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -128,7 +128,7 @@ The elements of :problems has attributes:
 (defconst leetcode--api-try                 (concat leetcode--base-url "/problems/%s/interpret_solution/"))
 
 
-(defsubst leetcode--referer (value)
+(defun leetcode--referer (value)
   "It will return an alist as the HTTP Referer Header.
 VALUE should be the referer."
   (cons "Referer" value))
@@ -165,7 +165,7 @@ VALUE should be the referer."
                   secret))
               (plist-get found :save-function)))))
 
-(defsubst leetcode--multipart-form-data (name value)
+(defun leetcode--multipart-form-data (name value)
   `("file"
     ("name"         . ,name)
     ("filedata"     . ,value)
